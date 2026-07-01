5b6d4538a78a5e3b5c660f206e47bdac5114fb5c
pmark22 <elpachano202@gmail.com>
2026-07-01 11:13:04 -0300

    Ruta GET /api/movies.

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 import express from "express";
+import sequelize from "./src/config/database.js";
+import "./src/models/movie.model.js";
+import movieRoutes from "./src/routes/movie.routes.js";
 
 const app = express();
 
-app.get("/api/movies", (req, res) => {
-    res.json({ ok: true });
-});
+app.use(express.json());
+
+// Rutas
+app.use("/api/movies", movieRoutes);
+
+try {
+    await sequelize.authenticate();
+    console.log("Base de datos conectada");
+
+    await sequelize.sync();
+    console.log("Tablas sincronizadas");
+} catch (error) {
+    console.error("Error al conectar la base de datos:", error);
+}
 
 app.listen(3000, () => {
-    console.log("SERVER ON 3000");
+    console.log("Servidor ejecutándose en puerto 3000");
 });
